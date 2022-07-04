1c12c06b41f2d878a96533dc16a496bcbce71ae4
Sakib Ahamed Khan <96885912+SakibAhamedKhan@users.noreply.github.com>
2022-07-04 21:43:08 +0600

    HOme page done

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import { async } from '@firebase/util';
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log(products);
   return (
     <div>
-      <div class="navbar bg-base-100">
+      <div class="navbar bg-base-100 px-5">
         <div class="flex-1">
           <a class="btn btn-ghost normal-case text-xl">E-Commerce</a>
         </div>
@@ -16,6 +18,34 @@ export default function Home() {
           </ul>
         </div>
       </div>
+      <div className='my-10 grid grid-cols-3 gap-4 mx-10'>
+        {
+          products.map(p => (
+            <div key={p._id} class="card w-full bg-base-100 shadow-xl justify-self-center hover:bg-base-200">
+              <figure class="px-10 pt-10">
+                <img src={p.image} alt="Shoes" class="rounded-xl" />
+              </figure>
+              <div class="card-body items-center text-center">
+                <h2 class="card-title">{p.name}</h2>
+                <p>{p.details}</p>
+                <div class="card-actions">
+                  <button class="btn btn-primary">Add Now</button>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const datafetch = await fetch('http://localhost:5000/products');
+  const data = await datafetch.json();
+  return {
+    props: {
+      products: data,
+    }
+  }
 }
